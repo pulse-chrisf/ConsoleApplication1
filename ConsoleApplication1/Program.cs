039c29fd9363c8214763593442da19255008fad4
chrisf <chrisf@pulsemicro.com>
2013-01-22 15:53:19 -0500

    Updates for Issue #1
    Console show the current time every second and runs until CTRL + C
    combination is entered.

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -1,15 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace ConsoleApplication1
 {
     class Program
     {
-        static void Main(string[] args)
+        private static bool _sStop;
+
+        public static void Main(string[] args)
         {
+            Console.CancelKeyPress += ConsoleCancelKeyPress;
+            while (!_sStop)
+            {
+                /* put real logic here */
+                Console.WriteLine("Still running at {0}", DateTime.Now);
+                Thread.Sleep(1000);
+            }
+        }
+
+        static void ConsoleCancelKeyPress(object sender, ConsoleCancelEventArgs e)
+        {
+            //you have 2 options here, leave e.Cancel set to false and just handle any
+            //graceful shutdown that you can while in here, or set a flag to notify the other
+            //thread at the next check that it's to shut down.  I'll do the 2nd option
+            e.Cancel = true;
+            _sStop = true;
+            Console.WriteLine("CancelKeyPress fired...");
+            Thread.Sleep(1000);
         }
     }
 }
